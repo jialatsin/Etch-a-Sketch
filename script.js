d173a33bd8a0e3333ae3157f4c38d6c736105290
jialatsin <singerardsin@gmail.com>
2022-07-19 19:04:17 +0800

    Updated slider and enabled sketching on main boxes

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let squares = document.querySelector('.squares');
+let size = document.querySelector('#myRange');
+let sizeNumber = document.querySelector('.size-number');
+if (size.addEventListener) {
+    size.addEventListener("mousemove", testtest, false);
+}
+else if (size.attachEvent) {
+    size.attachEvent('change', testtest);
+}
+function testtest(e) {
+    var value = e.target.value;
+    sizeNumber.textContent = value;
+}
+
+let mouseDown = false;
 
 for (let i=0; i<15; i++) {
     let row = document.createElement('div');
@@ -12,10 +26,18 @@ for (let i=0; i<15; i++) {
         row.appendChild(square);
         square.style.width = '100%';
         square.style.height = `${100/16}%`;
-        square.addEventListener('mouseover', function() {square.style.backgroundColor = 'black';}, false);
-        square.addEventListener('mouseout', function() {square.style.backgroundColor = 'white';}, false);
-    }
-   
-}
 
-console.log(squares.offsetWidth)
+        square.addEventListener('mousedown', function(){
+            square.style.backgroundColor = 'black';
+            mouseDown = true;
+        });
+        square.addEventListener("mousemove", function() {
+            if (mouseDown == true) {
+                square.style.backgroundColor = 'black';
+            }
+        });
+        square.addEventListener("mouseup", function(){
+            mouseDown = false;
+        })
+    }
+}
